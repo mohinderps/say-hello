d3fbb0cbae22ec418f2d611135370351ab84031b
mohinderps <mohin19derp@gmail.com>
2023-10-02 18:10:23 +0530

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import figlet from "figlet";
+import gradient from "gradient-string";
+import { program } from "commander";
+
+program.option("-n, --name <char>", "your name", "world");
+
+program.parse();
+
+const options = program.opts();
+
+figlet(`Hello, ${options.name} !`, function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(gradient.rainbow.multiline(data));
+});
